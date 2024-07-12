92b747c7aa280754474013b347ff2355b42b022a
Mark O Meara <markomeara98@gmail.com>
2024-07-12 16:09:06 +0100

    got basic tabs working

---
+++ rugbyApp/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,53 @@
+import { colors, fontSize } from "@/constants/tokens"
+import { BlurView } from "expo-blur"
+import { Tabs } from "expo-router"
+import { StyleSheet } from "react-native"
+import {FontAwesome, MaterialCommunityIcons, Ionicons, FontAwesome6} from '@expo/vector-icons'
+
+
+const TabsNavigation = () => {
+
+    return (
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: colors.primary,
+            tabBarLabelStyle: {
+                fontSize: fontSize.xs,
+                fontWeight: '500',
+            },
+            headerShown: false,
+            tabBarStyle: {
+                position: 'absolute',
+                borderTopLeftRadius: 20,
+                borderTopRightRadius: 20,
+                borderTopWidth: 0,
+                paddingTop: 8,
+            },
+        
+
+        }}>
+            <Tabs.Screen 
+            name="favourites" 
+            options={{
+                title: "Favourites",
+                tabBarIcon: ({color}) => <FontAwesome name="heart" size={20} color={color} />
+            }} />
+            <Tabs.Screen name="playlists" 
+                options={{
+                    title: "Playlists",
+                    tabBarIcon: ({color}) => <MaterialCommunityIcons name="playlist-play" size={28} color={color} />
+                }} />
+            <Tabs.Screen name="(songs)" 
+                options={{
+                    title: "Songs",
+                    tabBarIcon: ({color}) => <Ionicons name="musical-notes-sharp" size={24} color={color} />
+                }} />
+            <Tabs.Screen name="artists" 
+                options={{
+                    title: "Artists",
+                    tabBarIcon: ({color}) => <FontAwesome6 name="users-line" size={20} color={color} />
+                }} />
+        </Tabs>
+    )
+}
+
+export default TabsNavigation
--- rugbyApp/src/app/_layout.tsx
+++ rugbyApp/src/app/_layout.tsx
@@ -1,20 +1,23 @@
+import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Got it working!</Text>
-			<StatusBar style="auto" />
-		</View>
+const App = () => {
+	return <SafeAreaProvider>
+		<RootNavigation />
+
+		<StatusBar style='auto'/>
+	</SafeAreaProvider>
+
+}
+
+const RootNavigation = () => {
+	return(
+		<Stack>
+			<Stack.Screen  name='(tabs)' options={{headerShown: false}}/>
+		</Stack>
 	)
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-})
+export default App
